90999897188035a9f2a429d3f1d4556454e2f140
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-12 15:34:14 -0300

    Criado Data Acess Object e seus respectivos testes unitários

---
+++ src/main/java/br/com/arturtcs/desafioivia/dao/DaoGeneric.java
@@ -0,0 +1,62 @@
+package br.com.arturtcs.desafioivia.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.arturtcs.desafioivia.utils.HibernateUtil;
+
+public class DaoGeneric<T> {
+	
+	private EntityManager em = HibernateUtil.getEntityManager(); 
+	
+	public void salvar(T entidade) {
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.persist(entidade);
+		transaction.commit();
+	}
+	
+	public T pesquisar(Class<T> entidade, Long id) {
+		em.clear();
+		T entidadeRetornada = em.find(entidade, id);
+		
+		return entidadeRetornada;
+	}
+	
+	public T atualizar(T entidade) {
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		T entidadeSalva = em.merge(entidade);
+		transaction.commit();
+		return entidadeSalva;
+	}
+	
+	public void deletarPorId(T entidade) {
+		Object id = HibernateUtil.getPrimaryKey(entidade);
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		em.createNativeQuery(
+				"delete from " + entidade.getClass().getSimpleName().toLowerCase() + 
+				" where id = " + id).executeUpdate(); 
+		transaction.commit(); 
+	}
+	
+	public List<T> listarTodos(Class<T> entidade){
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		
+		List<T> lista = em.createQuery("from " + entidade.getName()).getResultList();
+		
+		transaction.commit();
+		
+		return lista;
+		
+	}
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+}
--- src/test/java/br/com/arturtcs/desafioivia/test/TesteCrud.java
+++ src/test/java/br/com/arturtcs/desafioivia/test/TesteCrud.java
@@ -1,5 +1,113 @@
 package br.com.arturtcs.desafioivia.test;
 
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.arturtcs.desafioivia.dao.DaoGeneric;
+import br.com.arturtcs.desafioivia.model.Telefone;
+import br.com.arturtcs.desafioivia.model.Usuario;
+
 public class TesteCrud {
+
+	@Test
+	public void salvarUsuario() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+		Usuario usuario = new Usuario();
+		usuario.setNome("Jorge Carlos");
+		usuario.setEmail("jorge@");
+		usuario.setSenha("123");
+		dao.salvar(usuario);
+		System.out.println("Pessoa cadastrada com sucesso!");
+	}
+
+	@Test
+	public void buscarUsuario() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+		Usuario usuario = new Usuario();
+		usuario = dao.pesquisar(Usuario.class, 1L);
+		System.out.println("O usuario pesquisado é o: " + usuario);
+	}
+
+	@Test
+	public void listarTodosUsuarios() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+		List<Usuario> lista = dao.listarTodos(Usuario.class);
+
+		for (Usuario usuarios : lista) {
+			System.out.println(usuarios);
+			System.out.println("---------------------------------------------------------------------------------");
+		}
+	}
+
+	@Test
+	public void atualizarUsuario() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+		Usuario usuarioPesquisado = dao.pesquisar(Usuario.class, 2L);
+
+		System.out.println("O usuário a ser alterado é o " + usuarioPesquisado);
+
+		usuarioPesquisado.setNome("Murilo Casé da Silva");
+
+		usuarioPesquisado = dao.atualizar(usuarioPesquisado);
+		System.out.println("Alterado nome do usuário para : " + usuarioPesquisado);
+	}
+
+	@Test
+	public void daoGenericDelete() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+		Usuario usuarioASerDeletado = dao.pesquisar(Usuario.class, 3L);
+
+		System.out.println("Usuário a ser deletado: " + usuarioASerDeletado);
+
+		dao.deletarPorId(usuarioASerDeletado);
+
+		System.out.println("Deletado com sucesso!");
+	}
+
+	@Test
+	public void testeGravaTelefone() {
+		DaoGeneric dao = new DaoGeneric();
+		Telefone fone = new Telefone();
+
+		Usuario usuario = (Usuario) dao.pesquisar(Usuario.class, 1L);
+
+		fone.setDdd(81);
+		fone.setTipo("fixo");
+		fone.setNumero("11111111");
+		fone.setUsuario(usuario);
+
+		dao.salvar(fone);
+
+		System.out.println("Telefone salvo com sucesso!");
+		System.out.println(fone);
+		System.out.println(usuario);
+
+	}
+
+	@Test
+	public void testeConsultaTelefone() {
+		DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+
+		Usuario pessoa = (Usuario) dao.pesquisar(Usuario.class, 1L);
+
+		for (Telefone telefones : pessoa.getTelefones()) {
+			System.out.println(telefones.getDdd());
+			System.out.println(telefones.getNumero());
+			System.out.println(telefones.getTipo());
+			System.out.println(telefones.getUsuario().getNome());
+			System.out.println("----------------------------------------------------------------------------");
+		}
+	}
+
+	@Test
+	public void listarTelefones() {
+		DaoGeneric<Telefone> dao = new DaoGeneric<Telefone>();
+		List<Telefone> lista = dao.listarTodos(Telefone.class);
+		for (Telefone telefones : lista) {
+			System.out.println(telefones);
+			System.out.println("-----------------------------------------------------------------------------");
+		}
+	}
 
 }
