e2593b7978a93c6fec1119f09cf371d8ca8ec0ce
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-12 13:36:45 -0300

    Entidades criadas

---
+++ src/main/java/br/com/arturtcs/desafioivia/model/Telefone.java
@@ -0,0 +1,110 @@
+package br.com.arturtcs.desafioivia.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefone implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private int ddd;
+	private String numero;
+	private String tipo;
+
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	private Usuario usuario;
+
+	public Telefone() {
+	}
+
+	public Telefone(Long id, int ddd, String numero, String tipo, Usuario usuario) {
+		super();
+		this.id = id;
+		this.ddd = ddd;
+		this.numero = numero;
+		this.tipo = tipo;
+		this.usuario = usuario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefone other = (Telefone) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Telefone [id=" + id + ", ddd=" + ddd + ", numero=" + numero + ", tipo=" + tipo + ", usuario=" + usuario
+				+ "]";
+	}
+
+}
