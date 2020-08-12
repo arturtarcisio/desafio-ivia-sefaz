2e3fda9d68bef839b7111ddbc5a56f06ab1dd431
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-12 20:38:58 -0300

    Criação de menu, pagina de cadastro e o managedBean de Usuario

---
+++ src/main/java/br/com/arturtcs/desafioivia/controller/UsuarioManagedBean.java
@@ -0,0 +1,83 @@
+package br.com.arturtcs.desafioivia.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.arturtcs.desafioivia.dao.DaoGeneric;
+import br.com.arturtcs.desafioivia.model.Usuario;
+
+@ManagedBean(name = "usuarioManagedBean")
+@ViewScoped
+public class UsuarioManagedBean {
+	
+	private Usuario usuario = new Usuario();
+	private List<Usuario> list = new ArrayList<Usuario>();
+	private DaoGeneric<Usuario> dao = new DaoGeneric<Usuario>();
+	
+	
+	public void init() {
+		list = dao.listarTodos(Usuario.class);
+	}
+	
+	public String salvar() {
+		dao.salvar(usuario);
+		list.add(usuario);
+		
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Usuário cadastrado com sucesso!"));
+		
+		return "";
+	}
+	
+	public String novo() {
+		usuario = new Usuario();
+		
+		return "";
+	}
+
+	public String excluirUsuario() {
+		
+		try {
+			dao.removerUsuarioCascata(usuario);
+			list.remove(usuario);
+			usuario = new Usuario();		
+			
+			FacesContext.getCurrentInstance().addMessage(null, 
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Excluído com sucesso!"));
+			
+		} catch (Exception e) {
+			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+				FacesContext.getCurrentInstance().addMessage(null, 
+						new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Existem telefones para o usuário!"));
+			} else {
+				e.printStackTrace();
+			}
+		}
+			
+		usuario = new Usuario();		
+		
+		return "";
+	}
+	
+	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public List<Usuario> getList() {
+		return list;
+	}
+	public void setList(List<Usuario> list) {
+		this.list = list;
+	}
+		
+
+}
