93fd111cb875567a9e18c75cd63e083b5ff2f018
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-12 20:45:35 -0300

    Criação DaoUsuario

---
+++ src/main/java/br/com/arturtcs/desafioivia/dao/DaoUsuario.java
@@ -0,0 +1,16 @@
+package br.com.arturtcs.desafioivia.dao;
+
+import br.com.arturtcs.desafioivia.model.Usuario;
+
+public class DaoUsuario<T> extends DaoGeneric<Usuario> {
+	
+	public void removerUsuario(Usuario usuario) throws Exception {
+		getEm().getTransaction().begin();
+		String sqlDeleteFone = "delete from TelefoneUser where usuariopessoa_id = " + usuario.getId();
+		getEm().createNativeQuery(sqlDeleteFone).executeUpdate();
+		getEm().getTransaction().commit();
+		
+		super.deletarPorId(usuario);
+	}
+
+}
