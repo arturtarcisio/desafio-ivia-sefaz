586248ed0a49a4dca8f00dc562ea741c2c804ece
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-13 16:42:51 -0300

    Criando rotina de login

--- src/main/java/br/com/arturtcs/desafioivia/controller/UsuarioManagedBean.java
+++ src/main/java/br/com/arturtcs/desafioivia/controller/UsuarioManagedBean.java
@@ -23,101 +23,113 @@ import br.com.arturtcs.desafioivia.model.Usuario;
 @ManagedBean(name = "usuarioManagedBean")
 @ViewScoped
 public class UsuarioManagedBean {
-	
+
 	private Usuario usuario = new Usuario();
 	private List<Usuario> list = new ArrayList<Usuario>();
 	private DaoUsuario<Usuario> dao = new DaoUsuario<Usuario>();
-	
+
 	@PostConstruct
 	public void init() {
 		list = dao.listarTodos(Usuario.class);
 	}
-	
+
+	public String logar() {
+		Usuario usuarioLogar = dao.login(usuario.getEmail(), usuario.getSenha());
+
+		if (usuarioLogar != null) {
+			// adicionar o usuário na sessão usuarioLogado
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuarioLogar);
+
+			return "usuarioCadastro.jsf";
+		}
+
+		return "index.jsf";
+	}
+
 	public String salvar() {
 		dao.salvar(usuario);
 		list.add(usuario);
-		
-		FacesContext.getCurrentInstance().addMessage(null, 
+
+		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Usuário cadastrado com sucesso!"));
-		
+
 		return "";
 	}
-	
+
 	public void pesquisaCep(AjaxBehaviorEvent event) {
 		try {
-			
+
 			URL url = new URL("https://viacep.com.br/ws/" + usuario.getCep() + "/json/");
 			URLConnection connection = url.openConnection();
 			InputStream is = connection.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-					
+
 			String cep = "";
 			StringBuilder jsonCep = new StringBuilder();
-			
-			while((cep = br.readLine()) != null) {
+
+			while ((cep = br.readLine()) != null) {
 				jsonCep.append(cep);
 			}
-			
+
 			Usuario usuarioCep = new Gson().fromJson(jsonCep.toString(), Usuario.class);
-			
+
 			usuario.setCep(usuarioCep.getCep());
 			usuario.setLogradouro(usuarioCep.getLogradouro());
 			usuario.setComplemento(usuarioCep.getComplemento());
 			usuario.setBairro(usuarioCep.getBairro());
 			usuario.setLocalidade(usuarioCep.getLocalidade());
 			usuario.setUf(usuarioCep.getUf());
-										
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	
-	
+
 	public String novo() {
 		usuario = new Usuario();
-		
+
 		return "";
 	}
 
 	public String excluirUsuario() {
-		
+
 		try {
-			dao.removerUsuarioCascata(usuario);;
+			dao.removerUsuarioCascata(usuario);
+			;
 			list.remove(usuario);
-			usuario = new Usuario();		
-			
-			FacesContext.getCurrentInstance().addMessage(null, 
+			usuario = new Usuario();
+
+			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Excluído com sucesso!"));
-			
+
 		} catch (Exception e) {
 			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-				FacesContext.getCurrentInstance().addMessage(null, 
-						new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Existem telefones para o usuário!"));
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Informação: ", "Existem telefones para o usuário!"));
 			} else {
 				e.printStackTrace();
 			}
 		}
-			
-		usuario = new Usuario();		
-		
+
+		usuario = new Usuario();
+
 		return "";
 	}
-	
-	
-	
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public List<Usuario> getList() {
 		return list;
 	}
+
 	public void setList(List<Usuario> list) {
 		this.list = list;
 	}
-		
 
 }
--- src/main/java/br/com/arturtcs/desafioivia/dao/DaoUsuario.java
+++ src/main/java/br/com/arturtcs/desafioivia/dao/DaoUsuario.java
@@ -1,16 +1,38 @@
 package br.com.arturtcs.desafioivia.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 import br.com.arturtcs.desafioivia.model.Usuario;
+import br.com.arturtcs.desafioivia.utils.HibernateUtil;
 
 public class DaoUsuario<T> extends DaoGeneric<Usuario> {
-	
+
 	public void removerUsuarioCascata(Usuario usuario) throws Exception {
 		getEm().getTransaction().begin();
 		String sqlDeleteFone = "delete from Telefone where usuario_id = " + usuario.getId();
 		getEm().createNativeQuery(sqlDeleteFone).executeUpdate();
 		getEm().getTransaction().commit();
-		
+
 		super.deletarPorId(usuario);
+	}
+
+	public Usuario login(String email, String senha) {
+
+		Usuario usuario = null;
+
+		EntityManager em = HibernateUtil.getEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+
+		usuario = (Usuario) em
+				.createQuery("select u from Usuario u where u.email = '" + email + "' and u.senha = '" + senha + "'")
+				.getSingleResult();
+
+		transaction.commit();
+		em.close();
+
+		return usuario;
 	}
 
 }
