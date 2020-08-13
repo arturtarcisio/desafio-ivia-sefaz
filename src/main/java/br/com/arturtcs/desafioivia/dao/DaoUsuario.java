74b1c4d61000f8db95e6a115254a3d1086a8d55b
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-08-12 21:04:37 -0300

    Alteraçao na página de cadastro e no DaoUsuario

--- src/main/java/br/com/arturtcs/desafioivia/dao/DaoUsuario.java
+++ src/main/java/br/com/arturtcs/desafioivia/dao/DaoUsuario.java
@@ -6,11 +6,13 @@ public class DaoUsuario<T> extends DaoGeneric<Usuario> {
 	
 	public void removerUsuarioCascata(Usuario usuario) throws Exception {
 		getEm().getTransaction().begin();
-		String sqlDeleteFone = "delete from TelefoneUser where usuariopessoa_id = " + usuario.getId();
+		String sqlDeleteFone = "delete from Telefone where usuariopessoa_id = " + usuario.getId();
 		getEm().createNativeQuery(sqlDeleteFone).executeUpdate();
 		getEm().getTransaction().commit();
 		
 		super.deletarPorId(usuario);
 	}
+	
+	
 
 }
